20be00db541f32192fc48037ded953c07a679b74
sethnificus <kuttnerseth@gmail.com>
2021-10-26 09:59:21 -0700

    update

---
+++ day2/lovecalculator.py
@@ -0,0 +1,13 @@
+# This part is for the input
+print("welcome to the Love Calculator!")
+name1 = input("What is your name? \n")
+name2 = input("what is their name? \n")
+
+#you should now have two variables.
+
+
+
+both_names = name1 + name2
+lowercase_both_names = both_names.lower()
+
+print(name1  +  name2)
