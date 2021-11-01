692f9c0df47881e6debdfb8ea1c3999911a829bc
sethnificus <kuttnerseth@gmail.com>
2021-11-01 15:55:02 -0700

    updates

--- week1/day3/BMI.py
+++ week1/day3/BMI.py
@@ -4,6 +4,14 @@ weight = float(input("enter your weight in kg: "))
 
 #This is my code
 
-if bmi <= 18.5
+bmi = weight / height
+
+
+if bmi <= 18.5:
     print("You are underweight")
-elif bmi >= 
+elif (bmi >= 18.5) and (bmi <= 25.0):
+    print("you are perfect weight!")
+elif (bmi > 25) and (bmi <= 35):
+    print("you are almost obese!")
+elif bmi > 35:
+    print("you are obese!")
