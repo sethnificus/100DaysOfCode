cd75a41f461d04b51d3d60d760006deaec3f31e2
sethnificus <kuttnerseth@gmail.com>
2021-11-02 16:57:05 -0700

    updates

--- week1/day3/BMI.py
+++ week1/day3/BMI.py
@@ -4,14 +4,16 @@ weight = float(input("enter your weight in kg: "))
 
 #This is my code
 
-bmi = weight / height
+bmi = round(weight / height ** 2)
 
 
 if bmi <= 18.5:
     print("You are underweight")
-elif (bmi >= 18.5) and (bmi <= 25.0):
+elif (bmi >= 18.5) and (bmi < 25.0):
     print("you are perfect weight!")
-elif (bmi > 25) and (bmi <= 35):
+elif (bmi >= 25) and (bmi < 30):
     print("you are almost obese!")
-elif bmi > 35:
+elif (bmi >= 30) and (bmi < 35):
     print("you are obese!")
+elif bmi > 35:
+    print("you are clinically obese!")
--- week2/day10/day10.py
+++ week2/day10/day10.py
@@ -1,9 +1,13 @@
 #Creating functions with outputs
 # 
 def name_function(f_name, l_name):
+    if f_name == "" or l_name == "":
+        return "you didn't provide a valid inputs"
+
+# using the .title function capitalizes the first letter. 
 
     formated_f_name = f_name.title()
     formated_l_name = l_name.title()
-    return f"{formated_f_name} {formated_l_name}"
+    return f"Results: {formated_f_name} {formated_l_name}"
 
-print(name_function ("SeTH", "KUTTner"))
+print(name_function (input("What is your first name? "), input("What is your last name? ")))
